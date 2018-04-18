60cbba43283f75298b17f5054e839049e0b9bad5
Jorge <jorge_mrl@hotmail.com>
2018-04-18 16:05:47 +0200

    Auto stash before merge of "master" and "origin/master"
    holi

--- content/authors.php
+++ content/authors.php
@@ -1,10 +1,8 @@
 <ul>
     Row 1
     <li>Rubennnnnnnn</li>
-    <li></li>
     <li>Noelia</li>
     <li>LaMary</li>
-    <li></li>
     <li>Javier</li>
     <li>Jorge</li>
 </ul>
@@ -13,11 +11,11 @@
     <li>Gordo18051994</li>
     <li>Jonas</li>
     <li>José (Gallego)</li>
-<<<<<<< Updated upstream
+
     <li>Zehio</li>
     <li>Frankt</li>
     <li>Jose Antonio Palomo</li>
-=======
+
 <<<<<<< Updated upstream
     <li>Zeio</li>
     <li>Francisco Trujillo</li>
