492e6a6d0e389f00255251fa5a5dae5efeb469ec
Unknown <juanmarojascardenas@gmail.com>
2018-04-18 15:47:17 +0200

    Subiendo Juanma

--- content/authors.php
+++ content/authors.php
@@ -13,15 +13,31 @@
     <li>Gordo18051994</li>
     <li>Jonas</li>
     <li>José (Gallego)</li>
+<<<<<<< Updated upstream
     <li>Zehio</li>
     <li>Francisco Trujillo</li>
     <li>Jose Antonio Palomo</li>
+=======
+<<<<<<< Updated upstream
+    <li>Zeio</li>
+    <li>Francisco Trujillo</li>
+=======
+    <li></li>
+    <li>Francisco Trujillo xD </li>
+>>>>>>> Stashed changes
+    <li></li>
+>>>>>>> Stashed changes
 </ul>
 <ul>
     Row 3
     <li></li>
     <li>Diego Andrades</li>
     <li></li>
+<<<<<<< Updated upstream
     <li>monetelab</li>
     <li></li>
+=======
+    <li></li>
+    <li>Juanma Rojas</li>
+>>>>>>> Stashed changes
 </ul>
