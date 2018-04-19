aae3c5ad9ebb34c101c83cf21ccff5e8418cf269
SergioAG <sergdalag@gmail.com>
2018-04-19 16:01:37 +0200

    Cambio Nombre Zehio a Sergio

--- content/authors.php
+++ content/authors.php
@@ -11,7 +11,7 @@
     Row 2
     <li>Gordo18051994</li>
     <li>José (Gallego)</li>
-    <li>Zehio</li>
+    <li>Sergio</li>
     <li>Francisco Trujillo</li>
     <li>Jose Antonio Palomo</li>
 </ul>
